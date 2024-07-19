1cf7e910a1d587045a2224fd1abedb4ac28b5794
11340526@qq.com <11340526@qq.com>
2024-07-19 14:19:18 +0800

    Base

---
+++ TypeScriptTutorail/Program.cs
@@ -0,0 +1,46 @@
+using Ganss.Xss;
+using Microsoft.FluentUI.AspNetCore.Components;
+using TypeScriptTutorail.Components;
+
+namespace TypeScriptTutorail
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents();
+            builder.Services.AddFluentUIComponents();
+
+            builder.Services.AddScoped<IHtmlSanitizer, HtmlSanitizer>(x =>
+            {
+                // Configure sanitizer rules as needed here.
+                // For now, just use default rules + allow class attributes
+                var sanitizer = new HtmlSanitizer();
+                sanitizer.AllowedAttributes.Add("class");
+                return sanitizer;
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>();
+
+            app.Run();
+        }
+    }
+}
