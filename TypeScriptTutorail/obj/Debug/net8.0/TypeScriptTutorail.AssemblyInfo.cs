7ecc84205d7bf0371e3d231bf6f82c875295c2e4
11340526@qq.com <11340526@qq.com>
2024-07-19 14:27:36 +0800

    hello

--- TypeScriptTutorail/obj/Debug/net8.0/TypeScriptTutorail.AssemblyInfo.cs
+++ TypeScriptTutorail/obj/Debug/net8.0/TypeScriptTutorail.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TypeScriptTutorail")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b54608fd51a48d782b338d2762e675076a40561a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dcf3a19ebb4da820f241e24a2f6fe3906a7c2ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("TypeScriptTutorail")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TypeScriptTutorail")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
